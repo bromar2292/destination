737a49d196402499c2076d1d2040a91cc112ab6d
Your Name <you@example.com>
2019-02-21 13:50:46 +0000

    initial index changes

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const Country = require("./countries.js");
+
+const PORT = 3500;
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(`This time is ${Date.now()}`);
+  next();
+});
+
+app.get("/countries", (req, res) => {
+  // let name = req.query.citys;
+  // if (!name) {
+  //     let allCountries = TheNames.allCountries
+  // }
+  res.json({ message: "hello, this will include countries" });
+});
+
+app.get("/cities", (req, res) => {
+  // let name = req.query.citys;
+  // if (!name) {
+  //     let allCountries = TheNames.allCountries
+  // }
+  res.json({ message: "hello, this will include cities" });
+});
+
+app.get("/language", (req, res) => {
+  // let name = req.query.citys;
+  // if (!name) {
+  //     let allCountries = TheNames.allCountries
+  // }
+  res.json({ message: "hello, this will include language" });
+});
+
+app.get("/cuisine", (req, res) => {
+  // let name = req.query.citys;
+  // if (!name) {
+  //     let allCountries = TheNames.allCountries
+  // }
+  res.json({ message: "hello, this will include cuisine" });
+});
+
+app.get("/tips", (req, res) => {
+  // let name = req.query.citys;
+  // if (!name) {
+  //     let allCountries = TheNames.allCountries
+  // }
+  res.json({ message: "hello, this will include travelers tips" });
+});
+
+app.get("/activites", (req, res) => {
+  // let name = req.query.citys;
+  // if (!name) {
+  //     let allCountries = TheNames.allCountries
+  // }
+  res.json({ message: "hello, this will include travelers tips" });
+});
+
+app.listen(PORT, () => {
+  console.log(`I'm listening on port ${PORT}`);
+});
