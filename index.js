12be7ae99baca17a54f6f0c0dc912041a0095ab1
karenchahal <karenjeet.chahal@gmail.com>
2019-02-21 16:47:06 +0000

    cors addition

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const PORT = 3000;
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use((req, res, next) => {
   console.log(`This time is ${Date.now()}`);
   next();
